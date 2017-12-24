170463978c81c678ade3a54f62752cc7b5bcfd6b
Suyash Nande <suyashnatural@gmail.com>
2017-12-24 16:08:52 -0500

    Additional Bank Manager Functionality

--- page_actions/customerdetails_page.js
+++ page_actions/customerdetails_page.js
@@ -1,5 +1,8 @@
 require('../utilities/customlocators.js');
 var BankMgrHomeLocators = require('../json/locators.json').BankMgrHomePage;
+var SelectWrapper = require('../utilities/select-wrapper');
+var selectCustomer = new SelectWrapper(by.model(BankMgrHomeLocators.Cust_DropDown_ngModel));
+var selectCurrency = new SelectWrapper(by.model(BankMgrHomeLocators.Currency_DropDown_ngModel));
 
 var customerdetails_page = function () {
     this.addCustomerInfo = function (fname, lname, postcode) {
@@ -9,15 +12,18 @@ var customerdetails_page = function () {
         element(by.model(BankMgrHomeLocators.PostCode_ngModel)).sendKeys(postcode);
         element(by.css(BankMgrHomeLocators.AddCust_Btn_css)).click();
     }
-    // this.openAnAccount = function (customer, currency) {
-    //     element(by.ngClick(BankMgrHomeLocators.Open_Acc_ngClick)).click();
-    //     element();
-    // }
-    this.goToSearchCustomer = function () {
-
+    this.openAnAccount = function (customer, currency) {
+        element(by.ngClick(BankMgrHomeLocators.Open_Acc_ngClick)).click();
+        selectCustomer.selectByText(customer);
+        selectCurrency.selectByText(currency);
+        element(by.buttonText(BankMgrHomeLocators.Process_btn_text)).click();
     }
-    this.validateCustomerRecords = function () {
-
+    this.SearchAndValidateCustomer = function (customer) {
+        element(by.ngClick(BankMgrHomeLocators.Show_Cust_ngClick)).click();
+        element(by.model(BankMgrHomeLocators.SearchBar_ngModel)).sendKeys(customer);
+    }
+    this.DeleteCustomerRecords = function () {
+        element(by.buttonText(BankMgrHomeLocators.Delete_btn_text)).click();
     }
 };
 module.exports = new customerdetails_page();
--- test_spec/BankManagerLogin_spec.js
+++ test_spec/BankManagerLogin_spec.js
@@ -5,10 +5,21 @@ describe('Bank Manager Login Test - ', function () {
     var data = require('../json/data.json');
     var logger = require('../utilities/log');
     var BankMgrData = require('../json/data.json').BankMgrHomePage;
+    var BankMgrHomeLocators = require('../json/locators.json').BankMgrHomePage;
     require('../page_actions/customerdetails_page');
 
-
     var customerdetails_page; //for reference
+
+    var handleAlert = function () {
+        var EC = protractor.ExpectedConditions;
+        browser.wait(EC.alertIsPresent(), 5000);
+
+        var alert = browser.switchTo().alert();
+        alert.getText().then(function (alertText) {
+            logger.log('info', alertText);
+        });
+        alert.accept();
+    };
 
     it('should login as bank manager', function () {
         basepage.navigateToUrl(locators.URL.loginPageURL);
@@ -20,26 +31,30 @@ describe('Bank Manager Login Test - ', function () {
         var title = basepage.getPageTitle();
         expect(title).toContain(data.LoginPage.Page_Title_Text);
         logger.log('info', 'validating the page title to contain: ' + data.LoginPage.Page_Title_Text);
-        browser.sleep(2000);
     });
 
     it('should add a customer', function () {
         customerdetails_page.addCustomerInfo(BankMgrData.FirstName, BankMgrData.LastName, BankMgrData.PostCode);
-        logger.log('info', 'customer info added');
-
-        var EC = protractor.ExpectedConditions;
-        browser.wait(EC.alertIsPresent(), 5000);
-        logger.log('info', 'alert present');
-
-        var alert = browser.switchTo().alert();
-            alert.getText().then(function(alertText){
-            logger.log('info', alertText);
-        });
-        alert.accept();
-        logger.log('info', 'alert accepted');
+        logger.log('info', 'Adding customer info');
+        handleAlert();
     });
 
     it('should open an account', function () {
-        
+        customerdetails_page.openAnAccount(BankMgrData.CustomerName, BankMgrData.Currency);
+        logger.log('info', 'Opening Customer Account');
+        handleAlert();
+    });
+
+    it('should search and validate a customer', function () {
+        customerdetails_page.SearchAndValidateCustomer(BankMgrData.Search_Cust_Name);
+        logger.log('info', 'Searching the customer: ' + BankMgrData.Search_Cust_Name);
+        var firstName = element(by.repeater(BankMgrHomeLocators.Customer_Loop).row(0).column('cust.fName'));
+        expect(firstName.getText()).toBe(BankMgrData.Search_Cust_Name);
+        logger.log('info', 'Customer validated successfully');
+    });
+
+    it('should delete the customer', function () {
+        customerdetails_page.DeleteCustomerRecords();
+        logger.log('info', 'Customer deleted successfully');
     });
 });
